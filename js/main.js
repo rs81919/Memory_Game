2aa6112136dec842d07e3e99556f303cba740d36
Smith <RS81919@AD1.PROD>
2018-05-03 10:53:18 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User Flipped " + cardOne);
 console.log("User Flipped " + cardTwo);
-// This is a test comment 
+if (cardsInPlay.length === 2) {
+    //This is to check if length equals 2
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    }
+        else  {
+            alert("Sorry, try again.");
+        }
+    }
+
+
