50ddb0d2455453ced60272c67136fe3f679ab289
Smith <RS81919@AD1.PROD>
2018-05-03 17:11:04 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "King"];
+var cards = [
+    {
+        card: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        card: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        card: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        card: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    },
+
+];
 var cardsInPlay = [];
 var checkForMatch = function()  {
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -9,16 +31,18 @@ var checkForMatch = function()  {
     }
 }
 var flipCard = function (cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cardId);
+    console.log("User flipped " + cards[cardId].card);
+    cardsInPlay.push(cards[cardId].card);
     if (cardsInPlay.length === 2) {
         //This is to check if length equals 
         checkForMatch();
     }
-    
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit) 
 };
 flipCard(0);
 flipCard(2);
+
 
 
 
